c464299bacc82e22a1f31babeaa8d726848a8e73
Jude <jrbelayro@up.edu.ph>
2019-10-28 01:06:07 +0800

    okiis

--- js-ex5-rcastor.js
+++ js-ex5-rcastor.js
@@ -2,14 +2,21 @@ window.onload = function () {
     console.log("heii");
 }
 
+
+
 $(".number").focus(function() {
     console.log("focused");
-    $(this).after("<div id=\"calc\">test<div>")
+    // $(this).after("<div id=\"calc\">test<div>");
+    $(this).after("<div id=\"calc\"><input type=\"text\" id=\"entry\"><button id=\"extract\" ()\">extract</button></div>");
+    $("#form").on("click", "#extract", function () {
+        let entry = $("#form > #calc > #entry").val();
+        console.log(entry);
+    });
 });
 
-$("#form > input").focus(function () {
-    $("form > #calc").remove();
-});
+// $("#form > input").focus(function () {
+//     $("form > #calc").remove();
+// });
 
 /* $(".number").blur(function () {
     $("#calc").remove();
