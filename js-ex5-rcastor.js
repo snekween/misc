91bab187fd551765587139c5aea40df8088f08f8
Jude <jrbelayro@up.edu.ph>
2019-10-27 22:44:58 +0800

    sort out appearance

--- js-ex5-rcastor.js
+++ js-ex5-rcastor.js
@@ -1,3 +1,20 @@
 window.onload = function () {
     console.log("heii");
 }
+
+$(".number").focus(function() {
+    console.log("focused");
+    $(this).after("<div id=\"calc\">test<div>")
+});
+
+$("#form > input").focus(function () {
+    $("form > #calc").remove();
+});
+
+/* $(".number").blur(function () {
+    $("#calc").remove();
+});
+ */
+$("#form").on("click", "#calc", function () {
+    console.log("dzaii");
+});
